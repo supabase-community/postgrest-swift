022e661877514cfc1fdefc24fa9a6bb732fba4f9
Guilherme Souza <grsouza@pm.me>
2021-09-01 10:01:30 -0300

    Add helper methods for serializing PostgrestResponse

--- Sources/PostgREST/PostgrestResponse.swift
+++ Sources/PostgREST/PostgrestResponse.swift
@@ -11,3 +11,18 @@ public struct PostgrestResponse: Hashable {
         self.count = count
     }
 }
+
+extension PostgrestResponse {
+
+    public func json() throws -> Any {
+        try JSONSerialization.jsonObject(with: data, options: [.allowFragments])
+    }
+
+    public func decoded<T: Decodable>(to type: T.Type = T.self, using decoder: JSONDecoder = JSONDecoder()) throws -> T {
+        try decoder.decode(type, from: data)
+    }
+
+    public func string(encoding: String.Encoding = .utf8) -> String? {
+        String(data: data, encoding: encoding)
+    }
+}

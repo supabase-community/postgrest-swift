85c8731e260904a3c73853b017f531109593a1d1
Guilherme Souza <grsouza@pm.me>
2021-09-14 06:25:17 -0300

    Add async/await support

---
+++ Sources/PostgREST/Concurrency.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+@available(iOS 15.0.0, macOS 12.0.0, *)
+extension PostgrestBuilder {
+  public func execute(head: Bool = false, count: CountOption? = nil) async throws -> PostgrestResponse {
+    try await withCheckedThrowingContinuation { continuation in
+      self.execute(head: head, count: count) { result in
+        continuation.resume(with: result)
+      }
+    }
+  }
+}

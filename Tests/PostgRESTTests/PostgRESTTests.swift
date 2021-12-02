dd0278b33a8be5cfe7d11a118d01f342e9c25980
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2021-12-02 10:02:25 -0600

    Finalize pulling changes from supabase

--- Tests/PostgRESTTests/PostgRESTTests.swift
+++ Tests/PostgRESTTests/PostgRESTTests.swift
@@ -2,14 +2,13 @@
 import XCTest
 
 final class PostgRESTTests: XCTestCase {
-    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
 //        XCTAssertEqual(PostgREST().text, "Hello, World!")
     }
-    
+
     static var allTests = [
         ("testExample", testExample),
     ]

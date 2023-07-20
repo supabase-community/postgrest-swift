20feaf7089cd2bd58abdb0ce472d699d5a24e6fc
Guilherme Souza <grsouza@pm.me>
2023-07-20 09:50:12 -0300

    Fix tests

--- Tests/PostgRESTTests/BuildURLRequestTests.swift
+++ Tests/PostgRESTTests/BuildURLRequestTests.swift
@@ -114,20 +114,36 @@
       XCTAssertNotNil(clientInfoHeader)
     }
   }
-#endif
 
 final class LockIsolated<Value>: @unchecked Sendable {
-  private let lock = NSLock()
-  private var value: Value
+  private let lock = NSRecursiveLock()
+  private var _value: Value
 
   init(_ value: Value) {
-    self.value = value
+    self._value = value
   }
 
   @discardableResult
-  func withValue<T>(_ block: (inout Value) -> T) -> T {
-    lock.lock()
-    defer { lock.unlock() }
-    return block(&value)
+  func withValue<T>(_ block: (inout Value) throws -> T) rethrows -> T {
+    try lock.sync {
+      var value = self._value
+      defer { self._value = value }
+      return try block(&value)
+    }
+  }
+
+  var value: Value {
+    lock.sync { self._value }
   }
 }
+
+extension NSRecursiveLock {
+  @discardableResult
+  func sync<R>(work: () throws -> R) rethrows -> R {
+    lock()
+    defer { unlock() }
+    return try work()
+  }
+}
+
+#endif
